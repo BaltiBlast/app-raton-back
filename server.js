99b5ba1c66531d35c419ee7e3831d4dbdaaf6688
BaltiBlast <fougeray.florian@gmail.com>
2025-05-31 21:03:42 +0200

    router init ok

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
+// NPM imports
 const express = require("express");
 require("dotenv").config();
-const server = express();
+
+// Local imports
+const router = require("./router");
 
 const { PORT } = process.env;
+const server = express();
+server.use(router);
 
 server.listen(PORT, () => {
   console.log(`The broche is turning on http://localhost:${PORT}`);
