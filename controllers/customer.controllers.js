3e87e3f0197f45860d26973a3959178cea442f23
BaltiBlast <fougeray.florian@gmail.com>
2025-06-05 00:24:04 +0200

    add customer ok. ( mapper + controller + route )

--- controllers/customer.controllers.js
+++ controllers/customer.controllers.js
@@ -1,10 +1,37 @@
+const { CustomerMapper } = require("../models/index.mapper");
+
 const customerControllers = {
-  getCustomers: (req, res) => {
-    console.log("GET");
+  addCustomer: async (req, res) => {
+    try {
+      const { customer_email, customer_name, customer_address, customer_phone } = req.body;
+      const { street_number, street_name, postal_code, city, country } = customer_address;
+
+      const userId = req.session.user._id;
+
+      const customerData = {
+        user_id: userId,
+        customer_name: customer_name,
+        customer_email: customer_email,
+        customer_address: {
+          street_number: street_number,
+          street_name: street_name,
+          postal_code: postal_code,
+          city: city,
+          country: country,
+        },
+        customer_phone: customer_phone,
+      };
+
+      const newCustomer = await CustomerMapper.addCustomer(customerData);
+      res.status(201).json({ success: true, data: newCustomer });
+    } catch (error) {
+      console.error("❌ Erreur création customer:", error);
+      res.status(500).json({ success: false, error: error.message });
+    }
   },
 
-  addCustomer: (req, res) => {
-    console.log("POST");
+  getCustomers: (req, res) => {
+    console.log("GET");
   },
 
   updateCustomer: (req, res) => {
--- models/customer.mapper.js
+++ models/customer.mapper.js
@@ -3,6 +3,11 @@ const customerSchema = require("../schemas/customers.schema");
 
 class CustomerMapper extends CoreMapper {
   customer = this.mongoose.model("Customer", customerSchema);
+
+  async addCustomer(customerData) {
+    const newCustomer = new this.customer(customerData);
+    return await newCustomer.save();
+  }
 }
 
 module.exports = CustomerMapper;
