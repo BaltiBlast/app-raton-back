b90ea5a23a864c789ff8d686efe78b2b887b107c
BaltiBlast <fougeray.florian@gmail.com>
2025-06-05 23:51:03 +0200

    add note ok

--- controllers/note.controllers.js
+++ controllers/note.controllers.js
@@ -3,7 +3,24 @@ const { NoteMapper } = require("../models/index.mapper");
 const noteControllers = {
   // ----------------------------------------------------------------------------------------------------- //
   // Add new note to DB
-  addNote: () => {},
+  addNote: async (req, res) => {
+    try {
+      const { note_title, note_content } = req.body;
+      const userId = req.session.user._id;
+
+      const noteDate = {
+        user_id: userId,
+        note_title: note_title,
+        note_content: note_content,
+      };
+
+      const newNote = await NoteMapper.addNote(noteDate);
+      res.status(201).json({ success: true, data: newNote });
+    } catch (error) {
+      console.error("❌ Erreur création note:", error);
+      res.status(500).json({ success: false, error: error.message });
+    }
+  },
 
   // ----------------------------------------------------------------------------------------------------- //
   // Update note to DB
