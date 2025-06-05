e7d3b428186c78482da9af99ab917cb005ad731e
BaltiBlast <fougeray.florian@gmail.com>
2025-06-05 23:28:51 +0200

    assets note feature

--- models/index.mapper.js
+++ models/index.mapper.js
@@ -1,8 +1,10 @@
 const mongoose = require("../configs/mongoDB");
 const User = require("./user.mapper");
 const Customer = require("./customer.mapper");
+const Note = require("./note.mapper");
 
 module.exports = {
   UserMapper: new User(mongoose),
   CustomerMapper: new Customer(mongoose),
+  NoteMapper: new Note(mongoose),
 };
